5140699c461a9c207d6477bdbeb83a17a640e303
petron <petron@Surface-Laptop-e6a66058.localdomain>
2024-03-27 13:49:34 +0100

    chage to json output

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 import time
 import json
+import yaml
+
+
 from flask import Flask,request,jsonify
 from openai import OpenAI
 from symbol_pair import pairs,supported_indicators
@@ -9,17 +12,122 @@ app = Flask(__name__)
 client = OpenAI(api_key=os.environ.get('OPENAI_KEY'))
 return_message = None
 
+with open("strategy_params.yml", 'r') as file:
+  # Read the file contents
+  strategy_params = yaml.safe_load(file)
+
 assistant = client.beta.assistants.create(
     name='strategy_generator',
     instructions=f'''
-        You are an assistant to help generate strategies for trading the following {pairs} in the Nigerian stock exchange market
-        Your can generated strategies using just the following supported indicators: {supported_indicators}.
-        Your are only allowed to discuss topics around Nigerian stock trading and generate strategies for Nigerian stocks.
-        When you are asked to generatie a strategy, your output should only be a python code of the strategy.
-        Do not make use of non builtin python packages in your strategy codes.
-        You are allowed to discuss stocks outside Nigeria and generate strategies for them if you have prior information on them.
+        You are FormaxionBot, an assistant on the Formaxion website that helps trader in generating strategies for stock trading on Nigerian stock Exchange (NGX).
+        Formaxion is a cutting-edge web platform revolutionizing the Nigerian stock trading landscape. Tailored for both seasoned investors and newcomers, 
+        traders can create strategies using drag and drop or prompt you to create strategy for them. the generated strategy will be sent backtesting engine 
+        for analysis so the strategy you generate must always be in json format. the currently supported indicators are current price, 
+        exponential moving average of price, moving average of price,  relative strength index, standard deviation of price. there will be support for other indicators in the future.
+        the building blocks of the strategies are asset, weight, conditional, filter which has its corresponding json representation. 
+
+        {pairs}is the company name and symbol pairing for stocks listed on the Nigerian Stock Exchange (NGX) . 
+
+        asset = {{
+            "type":"asset",
+            "symbol":"UBA"    
+        }}
+
+        weight = {{
+            "weight": "weight-specified", 
+            "blocks":[ ]
+        }}
+
+        options for weight  ["weight-equal", "weight-specified"]. must be 2 decimal places and sum must be equal to 1.
+
+        conditional = ((
+            "type": "conditional",
+            "condition": (
+                "subject": (
+                    "function":"moving-average-price", 
+                    "asset":"UBA", 
+                    "period": "1", 
+                    ),
+                "comparison": "greater-than", 
+                "predicate": (
+                    "type":"function", #options: ["function", "value"] 
+                    "function":"moving-average-price",
+                    "period": "1", #days
+                    "asset":"UBA",
+                    
+                    )
+                ),
+            "then": [
+                        (
+                    "type":"asset",
+                    "symbol":"UBA"    
+                    )
+                ],
+            "else": [
+                (
+                    "type":"asset",
+                    "symbol":"ZENITHBANK"    
+                )
+                ],
+        ))
+        option for function is ["current_price", "moving-average-price","relative-strength-index","exponential-moving-average-price","standard-deviation-price"]
+        option for comparison is ["greater-than", "less-than"]
+        filter = ((
+            "type": "filter",
+            "sort":()
+                "function":"moving-average-price",
+                "period": "1",
+                ),
+            "select":["top, 5"], #options: ["top, 5", "bottom, 5"] number could between 5 to 15
+            ))
+
+        example of stratgey using above blocks is strategy = ((
+            "name": "test1",
+            "description": "test1",
+            "rebalance_frequency": "weekly",
+            "weight": (
+                "type":"weight-specified",
+                "blocks":[
+                    (
+                    "value":0.65,
+                    "type":"asset",
+                    "symbol":"UBA"  
+                    ),
+                    (
+                        "value":0.35,
+                        "type": "conditional",
+                            "condition": ((
+                                "subject": (
+                                    "function":"moving-average-price",
+                                    "asset":"UBA",
+                                    "period": "1",
+                                    ),
+                                "comparison": "greater-than",
+                                "predicate": (
+                                    "type":"function",
+                                    "function":"moving-average-price",
+                                    "asset":"ZENITHBANK",
+                                    "period": "1",
+                                    )
+                                )),
+                            "then": [
+                                        (
+                                    "type":"asset",
+                                    "symbol":"ZENITHBANK"    
+                                    )
+                                ],
+                            "else": [
+                                (
+                                    "type":"asset",
+                                    "symbol":"CONOIL"    
+                                )
+                            ],
+                    )
+                ]
+            )
+
     ''',
-    model='gpt-3.5-turbo')
+    model='gpt-3.5-turbo-1106')
 
 threads = dict()
 
